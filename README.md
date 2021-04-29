0733b059a021a22e9fae68946d923c1d6089738a
Alvaro Cabanas <acabanas@newrelic.com>
2021-04-29 17:10:07 +0200

    Add golangci-lint and all the integrations

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ Usage and defaults:
   uses: alvarocabanas/static-analysis-configs-action@v1
   with:
     repository: 'nri-elasticsearch' # Required, name of the repository (nri-jmx, nri-nginx, infrastructure-agent, ...)
+    golangci: true
+    semgrep: true
 ```
--- action.sh
+++ action.sh
@@ -6,12 +6,26 @@ set -o pipefail
 # Populate defaults
 [[ -n $GITHUB_ACTION_PATH ]] || GITHUB_ACTION_PATH=$(pwd)
 
-if [[ -f "$GITHUB_ACTION_PATH"/semgrep/${REPOSITORY}.yml ]]
-then
-    echo "ℹ️ Copying semgrep files to repo root directory"
-    cp "$GITHUB_ACTION_PATH"/semgrep/${REPOSITORY}.yml .semgrep.yml
-    cp "$GITHUB_ACTION_PATH"/semgrep/.semgrepignore .semgrepignore
-else
-  echo "❌ Semgrep file does not exist for REPOSITORY '${REPOSITORY}'"
-  return 1
+if [ "${SEMGREP}" = "true" ]; then
+  if [[ -f "$GITHUB_ACTION_PATH"/semgrep/${REPOSITORY}.yml ]]
+  then
+      echo "ℹ️ Copying semgrep files to repo root directory"
+      cp "$GITHUB_ACTION_PATH"/semgrep/${REPOSITORY}.yml .semgrep.yml
+      cp "$GITHUB_ACTION_PATH"/semgrep/.semgrepignore .semgrepignore
+  else
+    echo "❌ Semgrep file does not exist for REPOSITORY '${REPOSITORY}'"
+    return 1
+  fi
+fi
+
+
+if [ "${GOLANGCI-LINT}" = "true" ]; then
+  if [[ -f "$GITHUB_ACTION_PATH"/golangci-lint/${REPOSITORY}.yml ]]
+  then
+      echo "ℹ️ Copying golangci-lint files to repo root directory"
+      cp "$GITHUB_ACTION_PATH"/golangci-lint/${REPOSITORY}.yml .golangci.yml
+  else
+    echo "❌ golangci-lint file does not exist for REPOSITORY '${REPOSITORY}'"
+    return 1
+  fi
 fi
