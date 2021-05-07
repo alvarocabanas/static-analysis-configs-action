9207a44a6c867acae9f2fb24e097f67e396125e5
Alvaro Cabanas <acabanas@newrelic.com>
2021-05-07 16:12:44 +0200

    test

--- action.sh
+++ action.sh
@@ -3,27 +3,21 @@
 set -o errexit
 set -o pipefail
 
-# Populate defaults
 [[ -n $GITHUB_ACTION_PATH ]] || GITHUB_ACTION_PATH=$(pwd)
+[[ -n $SEMGREP_APPEND ]] || SEMGREP_APPEND="false"
 
-if [ "${SEMGREP}" = "true" ]; then
-  echo "ℹ️ Copying semgrep files to repo root directory"
-  if [[ -f "$GITHUB_ACTION_PATH"/semgrep/${REPOSITORY}.yml ]]
+echo $GITHUB_ACTION_PATH
+ls -la
+
+find ./golangci-lint  -type f | while read -r file
+do
+  fileBasename=$(basename $file)
+
+  if [[ ! -f "$fileBasename" ]]
   then
-      cp "$GITHUB_ACTION_PATH"/semgrep/${REPOSITORY}.yml .semgrep.yml
+      echo "ℹ️ Copying $fileBasename file to repo root directory"
+      cp "$GITHUB_ACTION_PATH/$file" "$fileBasename"
   else
-      cp "$GITHUB_ACTION_PATH"/semgrep/default.yml .semgrep.yml
+      echo "ℹ️ Local $fileBasename file detected skipping overwrite"
   fi
-  cp "$GITHUB_ACTION_PATH"/semgrep/.semgrepignore .semgrepignore
-fi
-
-
-if [ "${GOLANGCI_LINT}" = "true" ]; then
-  echo "ℹ️ Copying golangci-lint files to repo root directory"
-  if [[ -f "$GITHUB_ACTION_PATH"/golangci-lint/${REPOSITORY}.yml ]]
-  then
-      cp "$GITHUB_ACTION_PATH"/golangci-lint/${REPOSITORY}.yml .golangci.yml
-  else
-      cp "$GITHUB_ACTION_PATH"/golangci-lint/default.yml .golangci.yml
-  fi
-fi
+done
